cace52a895cae3e79bf9e37fea1f7fe51d3af202
aday-allbase <maxroepz48@gmail.com>
2024-12-06 23:31:31 +0700

    Update build-x86-64.sh

--- package-openwrt/build-x86-64.sh
+++ package-openwrt/build-x86-64.sh
@@ -37,7 +37,7 @@ PACKAGES=""
     php8-cli php8-mod-bcmath php8-mod-calendar php8-mod-ctype php8-mod-curl php8-mod-dom php8-mod-exif \
     php8-mod-fileinfo php8-mod-filter php8-mod-gd php8-mod-iconv php8-mod-intl php8-mod-mbstring php8-mod-mysqli \
     php8-mod-mysqlnd php8-mod-opcache php8-mod-pdo php8-mod-pdo-mysql php8-mod-phar php8-mod-session \
-    php8-mod-xml php8-mod-xmlreader php8-mod-xmlwriter php8-mod-zip libopenssl-legacy"
+    php8-mod-xml php8-mod-xmlreader php8-mod-xmlwriter php8-mod-zip"
     
     # x86_64
     PACKAGES="$PACKAGES kmod-iwlwifi iw-full pciutils"
