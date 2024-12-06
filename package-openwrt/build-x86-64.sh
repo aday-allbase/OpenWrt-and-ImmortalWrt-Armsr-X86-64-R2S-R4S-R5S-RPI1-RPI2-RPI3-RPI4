ee2b872834e6152f19c4087b4c60ae9371521a79
aday-allbase <maxroepz48@gmail.com>
2024-12-06 23:25:59 +0700

    Update build-x86-64.sh

--- package-openwrt/build-x86-64.sh
+++ package-openwrt/build-x86-64.sh
@@ -21,7 +21,7 @@ PACKAGES=""
     PACKAGES="$PACKAGES coreutils-nohup bash iptables dnsmasq-full curl ca-certificates ipset ip-full iptables-mod-tproxy iptables-mod-extra libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag unzip luci-compat luci luci-base luci-app-openclash"
     
     # NAS and Hard disk tools
-    PACKAGES="$PACKAGES luci-app-diskman kmod-usb-storage kmod-usb-storage-uas ntfs-3g"
+    PACKAGES="$PACKAGES kmod-usb-storage kmod-usb-storage-uas ntfs-3g"
     
     # Bandwidth And Network Monitoring
     PACKAGES+=" internet-detector luci-app-internet-detector internet-detector-mod-modem-restart vnstat2 vnstati2 luci-app-vnstat2"
@@ -43,7 +43,7 @@ PACKAGES=""
     PACKAGES="$PACKAGES kmod-iwlwifi iw-full pciutils"
     
     # misc
-    PACKAGES="$PACKAGES zram-swap adb parted losetup resize2fs luci luci-ssl block-mount luci-app-ramfree htop bash curl wget wget-ssl tar unzip unrar gzip jq luci-app-ttyd nano httping screen openssh-sftp-server"
+    PACKAGES="$PACKAGES zram-swap adb parted losetup resize2fs luci luci-ssl block-mount htop bash curl wget wget-ssl tar unzip unrar gzip jq luci-app-ttyd nano httping screen openssh-sftp-server"
     
     PACKAGES="$PACKAGES -kmod-usb-net-rtl8152-vendor -procd-ujail -libgd -dnsmasq -automount -libustream-openssl -default-settings-chn -luci-i18n-base-zh-cn"
     
