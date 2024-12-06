f29e5b2f546a988184cd64e97da775ec24c38467
aday-allbase <maxroepz48@gmail.com>
2024-12-07 02:48:15 +0700

    aday

--- package-openwrt/build-x86-64.sh
+++ package-openwrt/build-x86-64.sh
@@ -16,9 +16,9 @@ PACKAGES=""
     kmod-usb-net-huawei-cdc-ncm kmod-usb-net-cdc-ether kmod-usb-net-rndis kmod-usb-net-sierrawireless kmod-usb-ohci kmod-usb-serial-sierrawireless \
     kmod-usb-uhci kmod-usb2 kmod-usb-ehci kmod-usb-net-ipheth usbmuxd libusbmuxd-utils libimobiledevice-utils usb-modeswitch kmod-nls-utf8 mbim-utils xmm-modem \
     kmod-phy-broadcom kmod-phylib-broadcom kmod-tg3"
-
+    
     # Tunnel option
-    PACKAGES="$PACKAGES coreutils-nohup bash iptables dnsmasq-full curl ca-certificates ipset ip-full iptables-mod-tproxy iptables-mod-extra libcap libcap-bin ruby ruby-yaml kmod-tun unzip luci-compat luci luci-base luci-app-openclash"
+    PACKAGES="$PACKAGES coreutils-nohup bash dnsmasq-full curl ca-certificates ipset ip-full libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag unzip kmod-nft-tproxy luci-compat luci luci-base luci-app-openclash"
     
     # NAS and Hard disk tools
     PACKAGES="$PACKAGES kmod-usb-storage kmod-usb-storage-uas ntfs-3g"
@@ -37,13 +37,13 @@ PACKAGES=""
     php8-cli php8-mod-bcmath php8-mod-calendar php8-mod-ctype php8-mod-curl php8-mod-dom php8-mod-exif \
     php8-mod-fileinfo php8-mod-filter php8-mod-gd php8-mod-iconv php8-mod-intl php8-mod-mbstring php8-mod-mysqli \
     php8-mod-mysqlnd php8-mod-opcache php8-mod-pdo php8-mod-pdo-mysql php8-mod-phar php8-mod-session \
-    php8-mod-xml php8-mod-xmlreader php8-mod-xmlwriter php8-mod-zip"
+    php8-mod-xml php8-mod-xmlreader php8-mod-xmlwriter php8-mod-zip libopenssl-legacy"
     
     # x86_64
     PACKAGES="$PACKAGES kmod-iwlwifi iw-full pciutils"
     
     # misc
-    PACKAGES="$PACKAGES zram-swap adb parted losetup resize2fs luci luci-ssl htop bash curl wget wget-ssl tar unzip unrar gzip jq luci-app-ttyd nano httping screen openssh-sftp-server"
+    PACKAGES="$PACKAGES zram-swap adb parted losetup resize2fs luci luci-ssl luci-app-ramfree htop bash curl wget wget-ssl tar unzip unrar gzip jq luci-app-ttyd nano httping screen openssh-sftp-server"
     
     PACKAGES="$PACKAGES -kmod-usb-net-rtl8152-vendor -procd-ujail -libgd -dnsmasq -automount -libustream-openssl -default-settings-chn -luci-i18n-base-zh-cn"
     
